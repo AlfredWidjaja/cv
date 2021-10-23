2db006a342b75a5a03fd57de4b069508f5079afb
AlfredWidjaja <65062658+AlfredWidjaja@users.noreply.github.com>
2021-10-23 14:02:33 +0700

    Personal cv website template

---
+++ public/js/function.js
@@ -0,0 +1,19 @@
+// All Event Listener for Website
+window.addEventListener("scroll", scrollable, stopped);
+
+document.getElementById("about_page").addEventListener("click", function(){
+    window.location.href = "about.html";
+});
+
+document.getElementById("porto-page").addEventListener("click", function(){
+    window.location.href = "portfolio.html";
+});
+//Function for scrollable navbar
+function scrollable(){
+    $("#navbar").css("background-color","#BA9A6D");
+    setTimeout("stopped()", 3000);
+}
+
+function stopped(){
+    $("#navbar").css({"background-color":"#DECAB6", "transition":"1s"});
+}
